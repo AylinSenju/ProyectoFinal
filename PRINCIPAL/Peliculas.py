aca15528a01b0e193645d1cca927751b5c2b7f9e
vanessa <vanesa.tapia@uabc.edu.mx>
2024-12-01 21:57:22 -0800

    .

--- PRINCIPAL/Peliculas.py
+++ PRINCIPAL/Peliculas.py
@@ -13,7 +13,6 @@ data["duracion_min"] = data["duracion"].str.extract(r"(\d+)").astype(float)
 
 def distribucion_calificaciones(data):
     fig = px.histogram(data, x="calificacion", nbins=10,
-                       title="Distribución de Calificaciones de Peliculas",
                        labels={"calificacion": "Calificación"},
                        text_auto=True)
 
@@ -35,7 +34,6 @@ def distribucion_generos(data):
         generos,
         names="genero",
         values="cantidad",
-        title="Distribucion de Generos de Peliculas",
         color="genero",
         template="plotly_dark"
     )
@@ -46,7 +44,6 @@ def estrenos_fecha(data):
     agrupado = data.groupby("estreno",as_index=False).size()
     fig_tres=px.line(agrupado,
                      x="estreno", y="size",
-                     title="Cantidad de Estrenos por Fecha",
                      labels={"estreno":"Fecha Estreno", "size":"Cantidad de Peliculas"})
     fig_tres.update_layout(
         xaxis_title="Fecha de Estreno",
@@ -65,7 +62,6 @@ def duracion_promedio_genero(data):
 
     fig_cuatro = px.bar(agrupado,
                  x="genero", y="duracion_min",
-                 title="Duracion Promedio de Peliculas por Genero",
                  labels={"genero":"Genero", "duracion_min":"Duracion promedio(minutos)"},
                  text="duracion_min"
                  )
@@ -104,7 +100,7 @@ def test():
         #FILA UNOOOO
         dbc.Row([
             dbc.Col([
-                html.H4("Distribucion de Calificaciones",style={"color":"#FFFFFF"}),
+                html.H4("Distribución de Calificaciones",style={"color":"#FFFFFF"}),
                 dcc.Graph(id="figDistCalificaciones")
             ],width=6),
             dbc.Col([
